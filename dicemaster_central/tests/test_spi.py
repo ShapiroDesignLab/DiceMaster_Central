5b16575797571f7e80231d0fb1f5843c6e9f4c5f
Huaidian Hou <danielhou315@gmail.com>
2025-08-07 04:14:38 +0100

    added two screen testing spi

--- dicemaster_central/tests/test_spi.py
+++ dicemaster_central/tests/test_spi.py
@@ -1,7 +1,6 @@
 import sys
 import time
-from time import perf_counter, sleep
-import random
+from time import perf_counter
 
 sys.path.append('/home/dice/DiceMaster/DiceMaster_Central/')
 from dicemaster_central.hw.screen import SPIDevice
@@ -16,26 +15,35 @@ current_path = os.path.dirname(os.path.abspath(__file__))
 # Setup SPI device
 
 spi_config = SPIConfig(
-    max_speed_hz=4800000,  # Example speed
-    mode=0b00,             # SPI Mode 0, not sure what that means
-    bits_per_word=8
+    max_speed_hz=96000,  # Example speed
 )
 
 # At this point, devices should be wired up. 
-spid = SPIDevice(
+spid1 = SPIDevice(
     bus_id=0,
-    bus_dev_id=1,
+    bus_dev_id=0,
     spi_config=spi_config,
     verbose=True
 )
+# spid2 = SPIDevice(
+#     bus_id=BUS_ID,
+#     bus_dev_id=1,
+#     spi_config=spi_config,
+#     verbose=True
+# )
 
 text_group = TextGroup(
     file_path=os.path.join(current_path, 'test_assets/hey_guys.json'),
 )
 text_msg = text_group.to_msg()
-spid.up()
-response = spid.send(text_msg.payload)
-spid.down()
+print(text_msg.payload)
+spid1.up()
+spid1.send(text_msg.payload)
+spid1.down()
+# spid2.up()
+# spid2.send(text_msg.payload)
+# spid2.down()
+
 print(f"Sent {len(text_msg.payload)} bytes")
 time.sleep(1)
 
@@ -46,27 +54,38 @@ image_media = Image(
     delay_time=255,
 )
 msgs = image_media.to_msg(rotation=Rotation(0))
-spid.up()
-spid.send(msgs[0].payload)
-spid.down()
-spid.up()
-spid.send_continuum([msg.payload for msg in msgs[1:]])
-spid.down()
+# Send spid1
+spid1.up()
+spid1.send(msgs[0].payload)
+spid1.down()
+spid1.up()
+spid1.send_continuum([msg.payload for msg in msgs[1:]])
+spid1.down()
+
+time.sleep(0.1)
+
+# Send spid2
+# spid2.up()
+# spid2.send(msgs[0].payload)
+# spid2.down()
+# spid2.up()
+# spid2.send_continuum([msg.payload for msg in msgs[1:]])
+# spid2.down()
+
 
 st = perf_counter()
 time.sleep(1)
 
-
 # Then test GIF
-gif_media = GIF(
-    file_path=os.path.join(current_path, f'test_assets/miss-you.gif.d'),
-    delay_time=100,
-)
-gif_msgs = gif_media.to_msg()
+# gif_media = GIF(
+#     file_path=os.path.join(current_path, f'test_assets/miss-you.gif.d'),
+#     delay_time=100,
+# )
+# gif_msgs = gif_media.to_msg()
 
-spid.up()
-for frame_msgs in gif_msgs:
-    for msg in frame_msgs:
-        spid.send(msg.payload)
-        sleep(0.05)
-spid.down()
+# spid.up()
+# for frame_msgs in gif_msgs:
+#     for msg in frame_msgs:
+#         spid.send(msg.payload)
+#         sleep(0.05)
+# spid.down()
